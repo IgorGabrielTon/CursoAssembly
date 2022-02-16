5448b52ff472efd2340d1318ddac5a2394183d36
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-02-16 00:00:45 -0300

    Update OlaMundo.asm

--- Aula01/OlaMundo.asm
+++ Aula01/OlaMundo.asm
@@ -1,14 +1,17 @@
 section .data
-  Msg DQ 'Olá Mundo Assembly!', 0x0a
-  Peso EQU $ - Msg
-  
- section .text
-  global main:
- 
- main:
-  MOV EAX, Peso
-  MOV EBX, Msg
-  int 0x80
-  
-  MOV EAX, 1
-  RET
+    msg db 'Olá Mundo Compuway!', 0xa ; Cria uma variável chamada msg do tipo Byte, e o 0xa finaliza ela!
+    tamanho equ $ - msg ; Calcula o peso da mensagem e coloca na variavel tamanho
+
+section .text
+    global _start
+
+_start:
+    mov edx, tamanho ; Coloca o tamanho da var no registrador EDX
+    mov ecx, msg ; Coloca a mensagem no registrador ECX
+    mov ebx, 1 ; Coloca a informação de saida de dados 1 no ebx
+    mov eax, 4; Coloca o comando escreva no EAX
+    int 0x80; Chama o Kernel (Sistema para executar o comando)
+    
+    mov eax, 1; Coloca o comando sair no EAX
+    int 0x80 ; Chama o Kernel
+    RET; Finaliza o rotulo
