cb786c0cc2d7eda8cc30dd85f1e5433da5305219
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-02-20 17:32:29 -0300

    Create CondicionalTeoria.asm

---
+++ Aula03/CondicionalTeoria.asm
@@ -0,0 +1,12 @@
+; If apenas compara sem nada inicialmente
+; cmp EAX, EBX
+; OPERADORES que saltam condicional
+; (je) =
+; (jg) >
+; (jge) >=
+; (jl) <
+; (jle) <=
+; (jne) !=
+; GoTo (jmp) SALTO OBRIGATORIO
+; Trazer números para os regeistradores usar DWORD[var]
+; 
